8fd862c172bb9cc6fc4a6b204b73ba48757126f5
peterliang2015 <happyqll@163.com>
2017-03-18 10:43:43 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FOCUS_VERSION=$1
+if [ x${FOCUS_VERSION} = x ]
+then
+    echo "Pls input focus version!!!"
+    exit 1
+fi
+
+echo "version input is ${FOCUS_VERSION}"
+
+filepath=`dirname $0`
+
+cd ${filepath}
+
+chmod +x services_ctl.sh
+
+docker build -t openscanner/wangwa:${FOCUS_VERSION} .
+
+cd -
